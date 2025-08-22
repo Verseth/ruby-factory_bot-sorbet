d3341587afb0ab52fb34f4078e5a2e98219c07b0
Mateusz Drewniak <matmg24@gmail.com>
2025-08-22 15:52:14 +0200

    Fix tapioca compiler

--- lib/tapioca/dsl/compilers/factory_bot.rb
+++ lib/tapioca/dsl/compilers/factory_bot.rb
@@ -26,23 +26,24 @@ module Tapioca
               next
             end
 
-            mod.create_method(
-              factory.name.to_s,
-              parameters: [
-                create_param('strategy', type: 'Symbol'),
-                create_rest_param('args', type: 'top'),
-                create_block_param('block', type: "T.proc.params(arg0: #{klass}).returns"),
-              ],
-            ) do |method|
+            mod.create_method(factory.name.to_s) do |method|
+              method.add_param('strategy')
+              method.add_rest_param('args')
+              method.add_block_param('block')
+
               method.add_sig do |sig|
+                sig.add_param('strategy', 'Symbol')
+                sig.add_param('args', 'top')
                 sig.add_param('block', 'NilClass')
                 sig.return_type = klass.to_s
               end
 
               method.add_sig do |sig|
                 sig.type_params << 'R'
-                sig.add_param('block', "T.proc.params(arg0: #{klass}).returns(T.type_param(:R))")
-                sig.return_type = 'T.type_param(:R)'
+                sig.add_param('strategy', 'Symbol')
+                sig.add_param('args', 'top')
+                sig.add_param('block', "T.proc.params(arg0: #{klass}).returns(T.type_parameter(:R))")
+                sig.return_type = 'T.type_parameter(:R)'
               end
             end
 
